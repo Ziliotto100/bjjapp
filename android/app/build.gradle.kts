edb17a2f7d5b6cfe446a310dbf55b1c05ff24496
Luis Ziliotto <luisziliotto@gmail.com>
2025-07-19 08:29:19 -0300

    Correção SDK solicitada pelo Play Console

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
     defaultConfig {
         applicationId = "com.ziliottosmartdev.matchbjj"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
